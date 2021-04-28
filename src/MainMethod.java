1cb122dafe4607883a97109e9a5450a92ff9590f
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-28 17:50:34 +0530

    Initial commit

---
+++ src/MainMethod.java
@@ -0,0 +1,29 @@
+public class MainMethod {
+    public static void main(String[] args) {
+
+    }
+    public boolean search(int[] arr,int searchElement){//this is the method signature which contains an array
+        // as input and an element to be seached.
+        boolean response=false;//we assign the ture and false to the varible as response
+        for (int i = 0; i < arr.length; i++) {//here we traverse in our array
+            // [(arr.fori) this is the shortcut key to implement for loop upto the length od array.
+            //now we compare each element with search element
+            if(arr[i] == searchElement){
+                //if found
+                response=true;
+                break;
+            }
+        }
+        return response;
+    }
+    //we have to create method for find the index at which the element is found if not return -1
+    public int findindex(int[] arr,int searchElement){
+        return-1;
+    }
+    //find the index of nth occurence of given element
+    public int findNthIndex(int[] arr,int givenElement,int n){
+        return -1;
+    }
+
+
+}
